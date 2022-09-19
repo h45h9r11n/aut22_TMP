2b79dad8fcefedfdce81d56862d34a3b45f61e5a
hashgriln <NHHanh.2610@gmail.com>
2022-09-19 19:00:50 +0300

    updated lab1b

---
+++ Lab1/Lab1b/index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is a title</title>
+    <script src="main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script>
+    <style><?php include 'style.css'; ?></style>
+</head>
+<body>  
+    <h2>This is a sentence.</h2>
+    This is also a sentence. <br>
+    <br><a href="./another.php">This is an another webpage</a><br>
+    <br><input type="button" value="Turn off protected mode" onclick="auth()"><br>
+
+</body>
+</html>
--- Lab1/Lab1b/main.js
+++ Lab1/Lab1b/main.js
@@ -1,7 +1,5 @@
-function stopCP() {
-    onselectstart = (e) => {
-        e.preventDefault();        
-    }
+function selectHandler(e) {
+    onselectstart = e.preventDefault();        
 }
 
 function stopPrntScr() {
@@ -19,22 +17,34 @@ function stopPrntScr() {
 
 function AccessClipboardData() {
     try {
-        window.clipboardData.setData('text', "Access   Restricted");
+        window.clipboardData.setData('text', "Access Restricted");
     } catch (err) {
     }
 }
 
-stopCP();
-
-document.addEventListener("keyup", function (e) {
+function keyupHandler(e){
     var keyCode = e.keyCode ? e.keyCode : e.which;
-        if (keyCode == 44) {
-            stopPrntScr();
-        }
-});
-
-setInterval("AccessClipboardData()", 300);
-window.location.assign("/another");
-function turnOff() {
-    window.location.assign("https://www.w3schools.com");
+    if (keyCode == 44) {
+        stopPrntScr();
+    }
 }
+
+
+document.addEventListener('selectstart', selectHandler);
+document.addEventListener("keyup", keyupHandler);
+setInterval("AccessClipboardData()", 300);   
+
+function auth(){
+    var password = "U2FsdGVkX1+Xa8/Rwga6AYAPv1xMoPGkL9bdI/5Gvu4=";
+    var input = prompt("Enter password to turn off protected mode",);
+    if (CryptoJS.AES.decrypt(password, "p4ssw0rdp4ssw0rd").toString(CryptoJS.enc.Utf8) == input){
+        document.removeEventListener('keyup', keyupHandler);
+        document.removeEventListener('selectstart', selectHandler);
+        alert("Turned off");
+    } else {
+        alert("Don't try to change things that don't belong to you!");
+    }
+}
+
+
+
