e57c204f3c3d21973f73e5639ae7c3c185758ff2
hashgriln <NHHanh.2610@gmail.com>
2022-10-23 20:30:16 +0300

    Done part 1

--- Lab3/secur/secur/Program.cs
+++ Lab3/secur/secur/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Win32;
 using System;
 using System.IO;
 using System.Security.AccessControl;
+using System.Security.Cryptography;
 using System.Security.Principal;
 
 namespace secur
@@ -11,40 +12,21 @@ namespace secur
     {
         static void SetFileSecurity(string FileName, string Account, FileSystemRights Rights, AccessControlType ControlType)
         {
-            // Create a new DirectoryInfo object.
             DirectoryInfo dInfo = new DirectoryInfo(FileName);
-
-            // Get a DirectorySecurity object that represents the
-            // current security settings.
             DirectorySecurity dSecurity = dInfo.GetAccessControl();
-
-            // Add the FileSystemAccessRule to the security settings.
             dSecurity.AddAccessRule(new FileSystemAccessRule(Account, Rights, ControlType));
-
-            // Set the new access settings.
             dInfo.SetAccessControl(dSecurity);
         }
 
         static void RemoveFileSecurity(string FileName, string Account, FileSystemRights Rights, AccessControlType ControlType)
         {
-            // Create a new DirectoryInfo object.
+
             DirectoryInfo dInfo = new DirectoryInfo(FileName);
-
-            // Get a DirectorySecurity object that represents the
-            // current security settings.
             DirectorySecurity dSecurity = dInfo.GetAccessControl();
-
-            // Add the FileSystemAccessRule to the security settings.
             dSecurity.RemoveAccessRule(new FileSystemAccessRule(Account, Rights, ControlType));
-
-            // Set the new access settings.
             dInfo.SetAccessControl(dSecurity);
         }
 
-        static bool CheckSignature()
-        {
-            return true;
-        }
         static void Main(string[] args)
         {
             string FileName = @".\sys.tat";
@@ -59,11 +41,18 @@ namespace secur
             {
                 Console.WriteLine("Enter address of registry: ");
                 string keyName = Console.ReadLine();
-                byte[] Signature = (byte[])Registry.GetValue(keyName, "Signature", new byte[] {});
-                if (CheckSignature()){
+                byte[] signedData = (byte[]) Registry.GetValue(keyName, "Signature", new byte[] {});
+                byte[] originalData = (byte[]) Registry.GetValue(keyName, "originalData", new byte[] { });
+                string publicKey = (string) Registry.GetValue(keyName, "publicKey", new byte[] { });
+
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.FromXmlString(publicKey);
+                Console.WriteLine("OK");
+                if (RSAalg.VerifyData(originalData, SHA256.Create(), signedData)){
+                    Console.WriteLine("Correct");
                     RemoveFileSecurity(FileName, Everyone, FileSystemRights.FullControl, AccessControlType.Deny);
                 }
-                   
+                Console.WriteLine();
             }
         }
 
